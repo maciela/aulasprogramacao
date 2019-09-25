c9ed7f44c9d755117c30af0dbef2370e3c40ff20
Maciela P. Rodrigues <35813357+macielarodrigues@users.noreply.github.com>
2019-09-25 00:41:21 -0300

    Update expressao_logica.py

--- expressao_logica.py
+++ expressao_logica.py
@@ -1,7 +1,4 @@
 # Expressão Lógica 
-num = int(input("Digite um número: "))
-faixa = 1<= num <=10
-if num == faixa:
-    print("False")
-else:
-    print("True")
+num = float(input("Digite um número: "))
+exp = 1 <= num or num >=10
+print(exp)
